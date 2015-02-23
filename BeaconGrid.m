81ef9d9247760c2e43eae6d01da05ed4821e9027
William Seaman <Bill@Williams-MacBook-Pro.local>
2015-02-23 13:49:53 -0800

    ui grid

--- BeaconGrid.m
+++ BeaconGrid.m
@@ -8,6 +8,10 @@
 
 #import "BeaconGrid.h"
 
+
+#define gridLineDistance 50
+#define kLogBase			2
+
 @implementation BeaconGrid
 
 
@@ -37,21 +41,30 @@
 
 - (void)buildGrid
 {
-        //draw horizontal lines
+    //draw horizontal lines
     CAShapeLayer *line = [CAShapeLayer layer];
     UIBezierPath *linePath=[UIBezierPath bezierPath];
     
-    [linePath moveToPoint: CGPointMake(_gridFrame.origin.x,100)];
-    //[linePath addLineToPoint:CGPointMake(_gridFrame.size.width, 100)];
     
-   [linePath addCurveToPoint:CGPointMake(_gridFrame.size.width, 100) controlPoint1:CGPointMake(300, 150) controlPoint2:CGPointMake(300, 150)];
     
-    line.path=linePath.CGPath;
-    line.fillColor = nil;
-    line.opacity = 1.0;
-    line.strokeColor = [UIColor redColor].CGColor;
-    [self addSublayer:line];
-
+    for(int x = 0;x<_gridFrame.size.height - x;x=x+gridLineDistance)
+    {
+        
+        int value = 100 + x;
+        
+        double location = value; //logValueForNumber(value, kLogBase) * value;
+        
+        [linePath moveToPoint: CGPointMake(_gridFrame.origin.x,location)];
+        //[linePath addLineToPoint:CGPointMake(_gridFrame.size.width, 100)];
+        
+        [linePath addCurveToPoint:CGPointMake(_gridFrame.size.width, location) controlPoint1:CGPointMake(10, 104 + x) controlPoint2:CGPointMake(152, 217 + x + (x/2))];
+        
+        line.path=linePath.CGPath;
+        line.fillColor = nil;
+        line.opacity = 1.0;
+        line.strokeColor = [UIColor darkGrayColor].CGColor;
+        [self addSublayer:line];
+    }
     
 }
 
